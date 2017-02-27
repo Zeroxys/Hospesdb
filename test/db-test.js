54802f2541d42f99e7be57d8e47d3841f87e4f55
Miguel Zavala <miguelzavalac@gmail.com>
2017-02-27 12:00:43 -0600

    Metodo para likes  agregado

--- test/db-test.js
+++ test/db-test.js
@@ -2,6 +2,7 @@ const test = require('ava')
 const Db = require('../')
 const uuid = require('uuid-base62')
 const r = require('rethinkdb')
+const fixtures = require('./fixtures')
 
 const DbName = `Hospesdb_${uuid.v4()}`
 const db = new Db({db: DbName})
@@ -31,40 +32,42 @@ test.after.always('eliminar la db de prueba', async t => {
 test('Test para guardar un blog', async t => {
   t.is(typeof db.saveBlog, 'function', 'deberia ser function')
 
-  let fixtures = {
-    url_img: `https://hospes.com/${uuid.v4()}`,
-    titulo: 'Titulo del blog',
-    contenido: 'Contenido del blog',
-    likes: 0,
-    liked: false,
-    blog_id: uuid.uuid()
-  }
+  let blog = fixtures.getBlog()
 
-  let created = await db.saveBlog(fixtures)
+  let created = await db.saveBlog(blog)
 
-  t.is(created.titulo, fixtures.titulo)
-  t.is(created.contenido, fixtures.contenido)
-  t.is(created.likes, fixtures.likes)
-  t.is(created.liked, fixtures.liked)
-  t.is(created.blog_id, fixtures.blog_id)
+  t.is(created.titulo, blog.titulo)
+  t.is(created.contenido, blog.contenido)
+  t.is(created.likes, blog.likes)
+  t.is(created.liked, blog.liked)
+  t.is(created.blog_id, blog.blog_id)
   t.is(typeof created.id, 'string')
   t.truthy(created.createdAt)
+  t.is(created.public_id, uuid.encode(created.id))
 })
 
-// Test para guardar trabajos
+// Test para darle like al blog
+test('Test para like blog', async t => {
+  t.is(typeof db.likeBlog, 'function', 'Deberia ser una funcion')
+
+  let blog = await fixtures.getBlog()
+  let created = await db.saveBlog(blog)
+  let result = await db.likeBlog(created.public_id)
+
+  t.true(result.liked)
+  t.is(result.likes, blog.likes + 1)
+})
+
+// Test para guardar los trabajos
 test('Test para guardar los trabajos', async t => {
   t.is(typeof db.saveWorks, 'function', 'Deberia ser una funcion')
 
-  let fixtures = {
-    url: [`https://hospes.com/works/work-01/${uuid.v4()}`, `https://hospes.com/works/work-02/${uuid.v4()}`],
-    titulo: 'Trabajo numero X',
-    descripcion: 'texto de descripcion de las imagenes'
-  }
+  let work = fixtures.getWork()
 
-  let created = await db.saveWorks(fixtures)
+  let created = await db.saveWorks(work)
 
-  t.deepEqual(created.url, fixtures.url)
-  t.is(created.titulo, fixtures.titulo)
-  t.is(created.descripcion, fixtures.descripcion)
+  t.deepEqual(created.url, work.url)
+  t.is(created.titulo, work.titulo)
+  t.is(created.descripcion, work.descripcion)
   t.truthy(created.createdAt)
 })
---
+++ test/fixtures/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const uuid = require('uuid-base62')
+
+const fixtures = {
+  getBlog () {
+    return {
+      url_img: `https://hospes.com/${uuid.v4()}`,
+      titulo: 'Titulo del blog',
+      contenido: 'Contenido del blog',
+      likes: 0,
+      liked: false,
+      blog_id: uuid.uuid()
+    }
+  },
+  getWork () {
+    return {
+      url: [`https://hospes.com/works/work-01/${uuid.v4()}`, `https://hospes.com/works/work-02/${uuid.v4()}`],
+      titulo: 'Trabajo numero X',
+      descripcion: 'texto de descripcion de las imagenes',
+      image_id: uuid.uuid()
+    }
+  }
+}
+
+module.exports = fixtures
